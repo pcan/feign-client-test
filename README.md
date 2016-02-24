4fc85f84141938ed49f70a8e10eb4c42a842378e
pcan <pierocangianiello@gmail.com>
2016-02-24 11:44:07 +0100

    minor updates.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Feign Client Test
 
 A Test project that uses Feign to upload Multipart files to a REST endpoint. Since Feign library does not support Multipart requests, I wrote a custom [`Encoder`](src/main/java/it/pcan/test/feign/client/FeignSpringFormEncoder.java)  that enables this feature, using a `HttpMessageConverter` chain that mimics Spring's `RestTemplate`.
+
+## Usage
+
+Clone repo and build with maven. Launch [`ServerMain`](src/main/java/it/pcan/test/feign/server/ServerMain.java) and then [`FeignClientMain`](src/main/java/it/pcan/test/feign/client/FeignClientMain.java).
--- src/main/java/it/pcan/test/feign/client/FeignClientMain.java
+++ src/main/java/it/pcan/test/feign/client/FeignClientMain.java
@@ -2,7 +2,9 @@ package it.pcan.test.feign.client;
 
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
+import it.pcan.test.feign.bean.UploadInfo;
 import it.pcan.test.feign.bean.UploadMetadata;
+import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -21,10 +23,21 @@ public class FeignClientMain {
 
         MultipartFile file = new InMemoryMultipartFile("filename.tmp", new byte[]{65, 66, 67, 68});
         MultipartFile file2 = new InMemoryMultipartFile("filename2.tmp", new byte[]{69, 70, 71, 72, 73, 68});
-        service.upload("customFolder", file, new UploadMetadata("pippo"));
-        service.uploadSimple("customFolder", file);
-        service.test(new UploadMetadata("pippo"));
-        service.uploadArray("customFolder", new MultipartFile[]{file, file2}, new UploadMetadata("pippo"));
+
+        UploadInfo test = service.test(new UploadMetadata("pippo"));
+        System.out.println("test: " + test);
+        
+        UploadInfo upload = service.upload("customFolder", file, new UploadMetadata("pippo"));
+        System.out.println("Upload test: " + upload);
+
+        UploadInfo uploadSimple = service.uploadSimple("customFolder", file);
+        System.out.println("UploadSimple test: " + uploadSimple);
+
+        List<UploadInfo> uploadArray = service.uploadArray("customFolder", new MultipartFile[]{file, file2}, new UploadMetadata("pippo"));
+        int i = 0;
+        for (UploadInfo uploadInfo : uploadArray) {
+            System.out.println("UploadArray test[" + (i++) + "]: " + uploadInfo);
+        }
 
     }
 
