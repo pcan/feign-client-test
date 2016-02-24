fb25da7a4bb8cd301e0f6b5ad7f7aad9d6506fb8
pcan <pierocangianiello@gmail.com>
2016-02-24 11:34:39 +0100

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# feign-client-test
+# Feign Client Test
+
+A Test project that uses Feign to upload Multipart files to a REST endpoint. Since Feign library does not support Multipart requests, I wrote a custom `Encoder` that enables this feature, using a `HttpMessageConverter` chain that mimics Spring's `RestTemplate`.
--- src/main/java/it/pcan/test/feign/server/ServerMain.java
+++ src/main/java/it/pcan/test/feign/server/ServerMain.java
@@ -2,16 +2,11 @@ package it.pcan.test.feign.server;
 
 import it.pcan.test.feign.bean.UploadInfo;
 import it.pcan.test.feign.bean.UploadMetadata;
-import java.awt.Dimension;
-import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
@@ -59,21 +54,7 @@ public class ServerMain {
     }
 
     public static void main(String[] args) throws Exception {
-
-        JFrame frame = new JFrame();
-        frame.setSize(new Dimension(300, 100));
-        frame.add(new JLabel("Close this window to stop context."));
-        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        java.awt.EventQueue.invokeLater(() -> frame.setVisible(true));
-
-        ConfigurableApplicationContext app = SpringApplication.run(ServerMain.class, args);
-        frame.addWindowListener(new java.awt.event.WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                app.close();
-            }
-        });
+        SpringApplication.run(ServerMain.class, args);
     }
 
 }
